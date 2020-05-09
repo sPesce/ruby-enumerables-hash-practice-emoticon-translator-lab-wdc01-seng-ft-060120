aa264a1a18b0b0ac1bda1d6acdf43b8b91fb1b57
stephen pesce <stevepesce879@gmail.com>
2020-05-09 01:30:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,37 @@
-# require modules here
+require 'pry'
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(filename)
+  emoticons = YAML.load_file(filename)
+  emoticons.reduce({}){|new, (key,valueArray)|
+    new[key] = {:english => valueArray[0], :japanese => valueArray[1]}
+    new 
+  }
+  #binding.pry 
+  
+  
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(filename, emoticon)
+  hash = load_library(filename)
+  eng_vers = ""
+  found = false
+  
+  while !found
+    hash.each{|(key_out, val_out)|
+      if(val_out[:japanese] == emoticon)
+        
+        eng_vers = val_out[:english]
+        found = true 
+      else
+        puts "none found, #{val_out}[:japanese] = #{val_out[:japanese]} != emoticon "
+      end 
+    }
+  end 
+  
+  
+ 
+  
 end
 
 def get_english_meaning
